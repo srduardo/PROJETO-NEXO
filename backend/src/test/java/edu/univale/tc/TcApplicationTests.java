e6fde64957d8d893ee6c5325f3ab4d6387dd370f
srduardo <eduardodev8@gmail.com>
2025-03-19 14:41:55 -0300

    chore: init

---
+++ backend/src/test/java/edu/univale/tc/TcApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.univale.tc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
